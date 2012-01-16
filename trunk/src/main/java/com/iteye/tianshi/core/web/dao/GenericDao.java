d466f038c7ea98aeb378f03f2ee75e9b8e5e1e36
jiangzx@yahoo.com <jiangzx@yahoo.com@7024cf79-774e-56da-fcbf-fb6f7ddc6da0>
2012-01-16 16:08:03 +0000

    增加可以多个字段排序的方法

--- trunk/src/main/java/com/iteye/tianshi/core/web/dao/GenericDao.java
+++ trunk/src/main/java/com/iteye/tianshi/core/web/dao/GenericDao.java
@@ -165,6 +165,17 @@ public interface GenericDao<E, PK extends Serializable> {
 	public List<E> findByPropertyAndOrder(String propertyName, Object value, String orderCol, SQLOrderMode orderMode);
 	
 	/**
+	 * 根据属性名称与值，查询出所有满足条件的持久化实体，并排序
+	 *
+	 * @param propertyName 属性名称
+	 * @param value 属性值
+	 * @param orderCol 排序字段
+	 * @param orderMode 排序模式（降序或升序）
+	 * @return 所有持久化实体的集合
+	 */
+	public List<E> findByPropertysAndOrders(String[] joinEntitys,String[] propertyNames, Object[] values, String[] orderCol, SQLOrderMode orderMode);
+	
+	/**
 	 * 根据多对属性名称与值，查询出所有满足条件的持久化实体，并排序
 	 *
 	 * @param propertyNames 属性名称
